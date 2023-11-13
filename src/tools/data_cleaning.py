0de8f6937fb4f3e9c3592cfebfe0eec1ed73280f
Jaurès Ememagagit config --global user.email mon.adresse@mail.comgit config --global user.email jememaga@gmail.commgit config --global user.name Jaurès <jordhy-jean-jaures.otogongoua-ememaga@etudiant.univ-reims.fr>
2023-11-13 14:11:04 +0100

    maj, retrait de la fonction read_file

--- src/tools/data_cleaning.py
+++ src/tools/data_cleaning.py
@@ -10,30 +10,6 @@ import unittest
 import os
 from typing import List, Tuple
 
-
-# Fonction pour charger un fichier csv
-
-def read_file(path : str) -> pd.DataFrame:
-    """
-    This function takes the way to a CSV or Excel file as a starter and returns a pandas dataframe.
-    
-    Parameters:
-        path (str): The path to the CSV or Excel file.
-        
-    Returns:
-        pd.DataFrame: A dataframe pandas containing the data from the file.
-    """
-    # Check the file extension to determine the format
-    if path.endswith('.csv'):
-        # Read a CSV file
-        df = pd.read_csv(path)
-    else:
-        # If the extension is not CSV, displays an error message
-        raise ValueError("File format not supported. Use a CSV file.")
-    
-    return df
-
-
 # Selection des colonnes utiles pour le calcul du Nutri-Score #
 
 list_all_columns = ["energy_100g", "fat_100g", "saturated-fat_100g", "trans-fat_100g", "cholesterol_100g",
--- tests/test_tools/test_data_cleaning.py
+++ tests/test_tools/test_data_cleaning.py
@@ -12,20 +12,7 @@ project_root = os.path.abspath(os.path.join(current_dir, "../.."))
 sys.path.append(project_root)
 
 # Importation des fonctions de data_cleaning.py
-from src.tools.data_cleaning import read_file, select_columns, split_train_test
-
-# test de la fonction read_file
-
-class TestReadFile(unittest.TestCase):
-    def test_read_csv_file(self):
-        path_file = "../../src/data/train.csv"
-        df_csv = read_file(path_file)
-        self.assertIsInstance(df_csv, pd.DataFrame, "La lecture du fichier CSV a échoué.")
-
-# test
-if __name__ == '__main__':
-    unittest.main(verbosity=2) # `verbosity` pour afficher un message d'erreur détaillé
-    
+from src.tools.data_cleaning import select_columns, split_train_test
     
 # test de la fonction select_columns 
 
