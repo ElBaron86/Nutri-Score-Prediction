8656cc33d2dccafea5e5cf3e9dbc8b3e050f2115
JauresEmemaga <jordhy-jean-jaures.otogongoua-ememaga@etudiant.univ-reims.fr>
2023-12-01 18:30:22 +0100

    ajout des logs et simplification de main

--- src/tools/main_random_forest.py
+++ src/tools/main_random_forest.py
@@ -6,8 +6,16 @@
  # @ Description: This script allows you to launch the construction of Random Forest producer or consumer models
  '''
 
+#### Imports ####
 import sys
 import os
+import pickle
+import pandas as pd
+import matplotlib.pyplot as plt
+import mplcyberpunk
+plt.style.use('cyberpunk')
+
+
 # Recovery of the current directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -15,98 +23,61 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 project_root = os.path.abspath(os.path.join(current_dir, "../.."))
 sys.path.append(project_root)
 
-#### Imports ####
-
-# standard library
-import time
-from typing import (List, Tuple)
-import pickle
-
-# Imports of third -party libraries
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-import seaborn as sns
-from scipy.stats import randint
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import (accuracy_score, confusion_matrix, precision_score, recall_score,
-                            classification_report, ConfusionMatrixDisplay
-                            )
-from sklearn.model_selection import RandomizedSearchCV, train_test_split
-
-import mplcyberpunk
-plt.style.use('cyberpunk')
-
 # Importing the functions of Make_random_Forest
-from src.tools.tools_random_forest import hyper_params_search, make_prediction, hyper_params_search_with_feature_elimination
+from src.tools.tools_random_forest import hyper_params_search, hyper_params_search_with_feature_elimination
 
 #### Data ####
-
 train, test = pd.read_csv(r'src\data\train.csv'), pd.read_csv(r'src\data\test.csv')
 
-# Numerisation des scores
-train['score'] = train['score'].map({"E":0, "D":1, "C":2, "B":3, "A":4})
-test['score'] = test['score'].map({"E":0, "D":1, "C":2, "B":3, "A":4})
+# Score digitization
+train['score'] = train['score'].map({"E": 0, "D": 1, "C": 2, "B": 3, "A": 4})
+test['score'] = test['score'].map({"E": 0, "D": 1, "C": 2, "B": 3, "A": 4})
 
 # Choice on the model to build 
-
 choice = input("Build the 'Producers' model (enter P) or build the 'Consumers' model (enter C)")
 if choice.lower() == 'p':
     X_train, X_test = train.drop(labels=['score'], axis=1), test.drop(labels=['score'], axis=1)
     
 elif choice.lower() == 'c':
-    X_train, X_test = train[['energy_100g',
-                                'fat_100g',
-                                'saturated-fat_100g',
-                                'carbohydrates_100g',
-                                'sugars_100g',  'proteins_100g',
-                                'salt_100g']], test[['energy_100g',
-                                'fat_100g',
-                                'saturated-fat_100g',
-                                'carbohydrates_100g',
-                                'sugars_100g',  'proteins_100g',
-                                'salt_100g']]
+    X_train= train[['energy_100g', 'fat_100g', 'saturated-fat_100g', 'carbohydrates_100g', 'sugars_100g', 'proteins_100g', 'salt_100g']] 
+    X_test =  test[['energy_100g', 'fat_100g', 'saturated-fat_100g', 'carbohydrates_100g', 'sugars_100g', 'proteins_100g', 'salt_100g']]
                                 
-else :
+else:
     print("Incorrect entry, please enter P or C")
-    
+
+# getting lables for train and test
 y_train, y_test = train['score'], test['score']
 
-
 # We offer a first optimal model with all basic variables according to the producer or consumer profile
-
-best_rf = hyper_params_search(X_train=X_train, y_train=y_train, n_iter = 30)
+best_rf = hyper_params_search(X_train=X_train, y_train=y_train, n_iter=2)
 
 # We are launching the search for an optimal model
-king_model, best_features = hyper_params_search_with_feature_elimination(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, actual_model = best_rf, 
-                                                                        n_cross_val=5, n_iter=30, 
+king_model, best_features = hyper_params_search_with_feature_elimination(X_train=X_train, y_train=y_train, X_test=X_test,
+                                                                        y_test=y_test, actual_model=best_rf, 
+                                                                        n_cross_val=5, n_iter=2, 
                                                                         n_estimators_min=15, n_estimators_max=300,
                                                                         max_depth_min=1, max_depth_max=20)
 
-        
-save_choice = input("Do you want to save the trained model? (Y/N)")
+# choose if you want to save the trained model. This will replace the previous version
+save_choice = input("Do you want to save the trained model? (Y/N). !!! This will replace the previous version")
 if save_choice.lower() == 'y':
     if choice.lower() == 'p':
         # Model recording (producer case) optimal in a pickle
         with open('random_forest_prod.pickle', 'wb') as content:
             pickle.dump(king_model, content)
         # Recording of the list of variables used for the optimal model    
-        with open('features_random_forest_prod.txt', 'w') as l:
+        with open('features_random_forest_prod.txt', 'w', encoding='utf-8') as l:
             for line in best_features:
-                l.write(f"{line}\n")
-                
-        print("#### Producer model successfully saved ! ####")
+                l.write(f"{line}\n")           
+        print("Producer model successfully saved!")
     else:
         # Optimal model (consumer case) model in a pickle
         with open('random_forest_conso.pickle', 'wb') as content:
             pickle.dump(king_model, content)
         # Recording of the list of variables used for the optimal model
-        with open('features_random_forest_conso.txt', 'w') as l:
+        with open('features_random_forest_conso.txt', 'w', encoding='utf-8') as l:
             for line in best_features:
                 l.write(f"{line}\n")
                 
-        print("#### Consumer model successfully saved ! ####")
-
-        
-print("#### Program completed ####")
-
+        print("Consumer model successfully saved!")
+print("Program completed")
--- src/tools/tools_random_forest.py
+++ src/tools/tools_random_forest.py
@@ -2,34 +2,48 @@
  # @ Author: Jaures Ememaga
  # @ Create Time: 2023-11-22 18:11:59
  # @ Modified by: Jaures Ememaga
- # @ Modified time: 2023-11-23 08:50:10
+ # @ Modified time: 2023-12-01 17:03:10
  # @ Description: This script brings together all functionalities to build Randomforest consumer and producer models
  '''
 
-#### Importations ####
+#### Imports ####
 
 # standard library
+import logging
 import time
 from typing import (List, Tuple)
 import pickle
+import sys
 
 # Imports of third -party libraries
 import matplotlib.pyplot as plt
+import mplcyberpunk
+plt.style.use('cyberpunk')
 import numpy as np
 import pandas as pd
 import seaborn as sns
 from scipy.stats import randint
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import (accuracy_score, confusion_matrix, precision_score, recall_score,
-                            classification_report, ConfusionMatrixDisplay
-                            )
-from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from sklearn.metrics import (accuracy_score, confusion_matrix, classification_report)
+from sklearn.model_selection import RandomizedSearchCV
 
-import mplcyberpunk
-plt.style.use('cyberpunk')
+# Set up logging
+logging.basicConfig(filename='src/tools/results_tool.log', level=logging.INFO)
 
+console_handler = logging.StreamHandler(sys.stdout)
+console_handler.setLevel(logging.ERROR)  # Set the level to ERROR to avoid printing INFO and WARNING messages
 
+# Create a formatter for subsequent log entries
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+console_handler.setFormatter(formatter)
 
+# Add the handler to the root logger
+logging.getLogger().addHandler(console_handler)
+
+# Log the system date and time as the first line in the log file
+with open('src/tools/results_tool.log', 'a') as log_file:
+    log_file.write(f"System Date and Time: {time.strftime('%Y-%m-%d %H:%M:%S')}\n")
+    
 #### Function for the search for hyperparameters ####
 
 def hyper_params_search(X_train : pd.DataFrame, y_train : pd.Series, n_cross_val : int = 5, n_iter : int = 30, n_estimators_min : int = 20,
@@ -53,8 +67,7 @@ def hyper_params_search(X_train : pd.DataFrame, y_train : pd.Series, n_cross_val
     """
     time_start = time.time()
     
-    print("#### Search for hyper parameters in progress... ####")
-    print()
+    logging.info("#### Search for hyper parameters in progress... ####")
     
     # Dictionary with the distributions of hyperparameter values to seek
     param_dist = {'n_estimators': randint(n_estimators_min, n_estimators_max),
@@ -73,11 +86,9 @@ def hyper_params_search(X_train : pd.DataFrame, y_train : pd.Series, n_cross_val
     best_rf = rand_search.best_estimator_ # Here the best model is that which has obtained a better average precision on all the Splits of the Cross Validation
     
     time_end = time.time()
-    print(f"Execution time : {time_end-time_start} secondes")
-    print()
-    print('Best hyperparameters:',  rand_search.best_params_)
+    logging.info(f"Execution time : {time_end - time_start} seconds")
+    logging.info('Best hyperparameters: %s', rand_search.best_params_)
     return best_rf
-
 
 
 #### Function for predicting and displaying detailed performance in the test ####
@@ -101,9 +112,8 @@ def make_prediction(model, X_test : pd.DataFrame, y_test : pd.Series, plot_conf_
     # Accuracy
     accuracy = accuracy_score(y_test, y_pred)
     
-    print(f"Accuracy : {accuracy}")
-    print()
-    print(classification_report(y_test, y_pred)) # Allows to have more complete information on the prediction carried out
+    logging.info(f"Accuracy : %s", accuracy)
+    logging.info('\n%s', classification_report(y_test, y_pred)) # Allows to have more complete information on the prediction carried out
     
     if plot_conf_mat:
         # Display of the confusion matrix in Heatmap format
@@ -158,8 +168,8 @@ def hyper_params_search_with_feature_elimination(X_train, y_train, X_test, y_tes
     removed_features = [] # List that will contain the variables withdrawn
     best_features = list(current_X_train.columns) # List of the best variables (currently those of the best known model)
     
-    print("#### Optimal model search by reduction of variables + search for hyper parameters in progress... ####")
-    print()
+    logging.info("#### Optimal model search by reduction of variables + search for hyper parameters in progress... ####")
+    
     while current_X_train.shape[1] > 2:
         feature_importances = best_model.feature_importances_ # We recover the importance of the variables of the best model
         weakest_feature_index = np.argmin(feature_importances) # we repeat the one with the lowest importance to remove it
@@ -167,7 +177,7 @@ def hyper_params_search_with_feature_elimination(X_train, y_train, X_test, y_tes
         if weakest_feature_index < current_X_train.shape[1]:
             removed_feature = current_X_train.columns[weakest_feature_index] # The least important variables are removed
             
-            print(f"Removed feature : {removed_feature}")
+            logging.info(f"Removed feature: {removed_feature}")
             
             current_X_train = current_X_train.drop(removed_feature, axis=1) # variable update by removing the least important
             current_X_test = current_X_test.drop(removed_feature, axis=1) # Also on test data
@@ -189,13 +199,11 @@ def hyper_params_search_with_feature_elimination(X_train, y_train, X_test, y_tes
             evolution_accuracy.append(new_accuracy)
             removed_features.append(removed_feature)
         else:
-            print("Error: weakest_feature_index is out of the limits.") # in case we could withdraw more than var
             break
     
     time_end = time.time()
-    print(f"Execution time : {time_end - time_start} seconds")
-    print('Best hyperparameters :', best_model.get_params())
-    print()
+    logging.info(f"Execution time {time_end - time_start} seconds : ")
+    logging.info(f"Best hyperparameters : {best_model.get_params()} ")
     
     plt.plot(range(1, len(evolution_accuracy) + 1), evolution_accuracy, marker='o')
     for i, txt in enumerate(removed_features):
