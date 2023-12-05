dd05401e9116a60f68a851018ce3e84ad64f9b0a
JauresEmemaga <jordhy-jean-jaures.otogongoua-ememaga@etudiant.univ-reims.fr>
2023-12-05 21:18:55 +0100

    ajout de quelques tests unitaires, optimisation des codes pour une note excellente pylint, revue du dossier tests.

--- src/tools/tools_random_forest.py
+++ src/tools/tools_random_forest.py
@@ -9,26 +9,25 @@
 #### Imports ####
 
 # standard library
+import sys
 import logging
 import time
-from typing import (List, Tuple)
-import pickle
-import sys
-
-# Imports of third -party libraries
-import matplotlib.pyplot as plt
-import mplcyberpunk
-plt.style.use('cyberpunk')
+from typing import List, Tuple
+# Imports of third-party libraries
 import numpy as np
 import pandas as pd
 import seaborn as sns
 from scipy.stats import randint
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import (accuracy_score, confusion_matrix, classification_report)
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.model_selection import RandomizedSearchCV
+import matplotlib.pyplot as plt
+import mplcyberpunk
+plt.style.use('cyberpunk')
 
 # Set up logging
-logging.basicConfig(filename='src/tools/results_tool.log', level=logging.INFO)
+LOG_FILE_PATH = 'results_tool.log'
+logging.basicConfig(filename=LOG_FILE_PATH, level=logging.INFO)
 
 console_handler = logging.StreamHandler(sys.stdout)
 console_handler.setLevel(logging.ERROR)  # Set the level to ERROR to avoid printing INFO and WARNING messages
@@ -41,226 +40,130 @@ console_handler.setFormatter(formatter)
 logging.getLogger().addHandler(console_handler)
 
 # Log the system date and time as the first line in the log file
-with open('src/tools/results_tool.log', 'a') as log_file:
+with open(LOG_FILE_PATH, 'a', encoding="utf-8") as log_file:
     log_file.write(f"System Date and Time: {time.strftime('%Y-%m-%d %H:%M:%S')}\n")
-    
-#### Function for the search for hyperparameters ####
 
-def hyper_params_search(X_train : pd.DataFrame, y_train : pd.Series, n_cross_val : int = 5, n_iter : int = 30, n_estimators_min : int = 20,
-                        n_estimators_max : int = 300, max_depth_min : int = 1, max_depth_ax : int = 20) -> RandomForestClassifier: 
-                        
-    """The 'hyper_params_Search' function is looking for hyperparameters for a Randomforest classifier.
-    
-    Args:
-        X_train (pd.DataFrame) : Explanatory training variables.
-        y_train (pd.Series) : Training labels.
-        n_cross_val (int): Number of data divisions in the crossed validation. D 5efault
-        n_iter (int): Number of iterations of the parameter search process = number of combinations of hyperparameters that will be tested.
-        The greater this number, the more time will take time, so pay attention to the fixed number. Default 30
-        n_estimators_min (int): Minimum number of trees in the research interval. Default 20.
-        n_estimators_max (int): Maximum number of trees in the research interval. Default 300.
-        max_depth_min (int): Minimum number of levels (depth) of trees in the research interval. Default 1.
-        max_depth_max (int): Maximum number of levels (depth) of trees in the search interval. Default 20.
-        
-    Returns:
-        best_rf (RandomForestClassifier): Best model obtained
-    """
+# Function for the search of hyperparameters
+def hyper_params_search(x_train: pd.DataFrame, y_train: pd.Series, n_cross_val: int = 5, n_iter: int = 30,
+                        n_estimators_max: int = 300) -> RandomForestClassifier:
+    """The 'hyper_params_Search' function is looking for the best hyperparameters for a RandomForest classifier."""
+
+    if x_train.empty or y_train.empty:
+        raise ValueError("Input dataset is empty.")
+
     time_start = time.time()
-    
+
     logging.info("#### Search for hyper parameters in progress... ####")
-    
+
     # Dictionary with the distributions of hyperparameter values to seek
-    param_dist = {'n_estimators': randint(n_estimators_min, n_estimators_max),
-              'max_depth': randint(max_depth_min, max_depth_ax)}
-    
+    param_dist = {'n_estimators': randint(x_train.shape[1], n_estimators_max),
+                  'max_depth': randint(1, x_train.shape[1] + 5)}
+
     rf = RandomForestClassifier()
 
-    # Hyperparameters randomness
-    rand_search = RandomizedSearchCV(rf, 
-                                    param_distributions = param_dist, 
-                                    n_iter=n_iter, 
-                                    cv=n_cross_val)
+    rand_search = RandomizedSearchCV(rf, param_distributions=param_dist, n_iter=n_iter, cv=n_cross_val)
 
     # Model adjustment to data
-    rand_search.fit(X_train, y_train)
-    best_rf = rand_search.best_estimator_ # Here the best model is that which has obtained a better average precision on all the Splits of the Cross Validation
-    
+    rand_search.fit(x_train, y_train)
+    best_rf = rand_search.best_estimator_
+
     time_end = time.time()
-    logging.info(f"Execution time : {time_end - time_start} seconds")
+    logging.info("Execution time : %s seconds", time_end - time_start)
     logging.info('Best hyperparameters: %s', rand_search.best_params_)
     return best_rf
 
-
-#### Function for predicting and displaying detailed performance in the test ####
-
-def make_prediction(model, X_test : pd.DataFrame, y_test : pd.Series, plot_conf_mat : bool = False) -> np.array:
-    """Function to make predictions with a Random Frorest model.
-
-    Args:
-        model :Model to use for prediction
-        X_test (pd.):Explanatory test variables
-        y_test (pd.Series) : test labels
-        plot_conf_mat (bool, optional): Show the confusion matrix in Heatmap format.
-
-    Returns:
-        y_pred (np.array) : Matrix containing the predicted classes by Model
-    """
-    
-    # Prediction
-    y_pred = model.predict(X_test)
-    
-    # Accuracy
+# Function for predicting and displaying detailed performance in the test
+def make_prediction(model, x_test: pd.DataFrame, y_test: pd.Series, plot_conf_mat: bool = False) -> np.array:
+    """Function to make predictions with a Random Forest model."""
+    y_pred = model.predict(x_test)
     accuracy = accuracy_score(y_test, y_pred)
-    
-    logging.info(f"Accuracy : %s", accuracy)
-    logging.info('\n%s', classification_report(y_test, y_pred)) # Allows to have more complete information on the prediction carried out
-    
+
+    logging.info("Accuracy: %s", accuracy)
+    logging.info("\n%s", classification_report(y_test, y_pred))
+
     if plot_conf_mat:
-        # Display of the confusion matrix in Heatmap format
         cm = confusion_matrix(y_test, y_pred)
         labels = ['E', 'D', 'C', 'B', 'A']
-        
+
         plt.figure(figsize=(8, 6))
         sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=labels, yticklabels=labels)
         plt.title('Confusion matrix')
         plt.xlabel('Predicted labels')
         plt.ylabel('True labels')
         plt.show()
+
     return y_pred
 
+# Optimal model search with variable reduction (backward_selection) and hyperparameters search
+def hyper_params_search_with_feature_elimination(x_train: pd.DataFrame, y_train: pd.Series, x_test: pd.DataFrame,
+                                                 y_test: pd.Series, actual_model: RandomForestClassifier,
+                                                 n_cross_val: int = 5, n_iter: int = 30,
+                                                 n_estimators_max: int = 300,
+                                                 plot_graph: bool = False) -> Tuple[RandomForestClassifier, List[str]]:
+    """Function to make a selection of models by gradually removing the variables with the lowest importance."""
 
-#### Optimal model search with variable reduction (backward_selection) and hyperparameters search #### 
+    if x_train.empty or y_train.empty:
+        raise ValueError("Input dataset is empty.")
 
-def hyper_params_search_with_feature_elimination(X_train, y_train, X_test, y_test, actual_model : RandomForestClassifier,
-                                                n_cross_val=5, n_iter=30, 
-                                                n_estimators_min=20, n_estimators_max=300, 
-                                                max_depth_min=1, max_depth_max=20) -> Tuple[RandomForestClassifier, List[str]]:
-    
-    """Function to make a selection of models by gradually removing the variables with the lowest importance.
-    
-    Args:
-        X_train (pd.DataFrame): Train explanatory variables.
-        y_train (pd.Series): Train labels.
-        X_test (pd.DataFrame): Explanatory test variables.
-        y_test (pd.Series): Test labels.
-        actual_model (RandomForestClassifier): Best current model
-        n_cross_val (int): Number of Splits to do in the crossed validation.
-        n_iter (int): Number of iterations of the parameter search process = number of combinations of hyperparameters that will be tested.
-        n_estimators_min (int): Minimum number of trees in the research interval. Default 20.
-        n_estimators_max (int): Maximum number of trees in the research interval. Default 300.
-        max_depth_min (int): Minimum number of levels (depth) of trees in the research interval. Default 1.
-        max_depth_max (int): Maximum number of levels (depth) of trees in the search interval. Default 20.
-        
-    Returns:
-        Tuple[RandomForestClassifier, List[str]]: Best model obtained and list of important variables
-    """
     time_start = time.time()
-    
-    current_X_train = X_train.copy()
-    current_X_test = X_test.copy()
-    
+
+    current_x_train = pd.DataFrame(x_train.copy())
+    current_x_test = pd.DataFrame(x_test.copy())
+
     # Initialize the best model with the model leads to you
     best_model = actual_model
-    
-    # Initialiosation of variables for iterations
-    best_accuracy = accuracy_score(y_test, best_model.predict(current_X_test)) # Best precise accuracy at the start (that of preceding best_rf)
-    evolution_accuracy = [best_accuracy] # accuracy list
-    removed_features = [] # List that will contain the variables withdrawn
-    best_features = list(current_X_train.columns) # List of the best variables (currently those of the best known model)
-    
+
+    # Initialisation of variables for iterations
+    best_accuracy = accuracy_score(y_test, best_model.predict(current_x_test))
+    evolution_accuracy = [best_accuracy]
+    removed_features = []
+    best_features = list(current_x_train.columns)
+
     logging.info("#### Optimal model search by reduction of variables + search for hyper parameters in progress... ####")
-    
-    while current_X_train.shape[1] > 2:
-        feature_importances = best_model.feature_importances_ # We recover the importance of the variables of the best model
-        weakest_feature_index = np.argmin(feature_importances) # we repeat the one with the lowest importance to remove it
-        
-        if weakest_feature_index < current_X_train.shape[1]:
-            removed_feature = current_X_train.columns[weakest_feature_index] # The least important variables are removed
-            
-            logging.info(f"Removed feature: {removed_feature}")
-            
-            current_X_train = current_X_train.drop(removed_feature, axis=1) # variable update by removing the least important
-            current_X_test = current_X_test.drop(removed_feature, axis=1) # Also on test data
-            
-            # We create a new model with the best characteristics and we train it
-            new_best_model = hyper_params_search(current_X_train, y_train, n_cross_val, n_iter, 
-                                                 n_estimators_min, n_estimators_max, 
-                                                 max_depth_min, max_depth_max)
-            new_best_model.fit(current_X_train, y_train)
-            
-            new_accuracy = accuracy_score(y_test, new_best_model.predict(current_X_test)) 
-            
-            # Update the best model and the best characteristics if only the precision is better
-            if new_accuracy > best_accuracy:
-                best_model = new_best_model
-                best_accuracy = new_accuracy
-                best_features = list(current_X_train.columns)
-            
-            evolution_accuracy.append(new_accuracy)
-            removed_features.append(removed_feature)
+
+    while current_x_train.shape[1] > 2:
+        feature_importances = best_model.feature_importances_
+        weakest_feature_index = np.argmin(feature_importances)
+
+        if weakest_feature_index < current_x_train.shape[1]:
+            removed_feature = current_x_train.columns[weakest_feature_index]
+
+            # Check if the removed feature is present in the current DataFrame
+            if removed_feature in current_x_train.columns:
+                logging.info("Removed feature: %s", removed_feature)
+
+                current_x_train = current_x_train.drop(removed_feature, axis=1, errors='ignore')
+                current_x_test = current_x_test.drop(removed_feature, axis=1, errors='ignore')
+
+                new_best_model = hyper_params_search(current_x_train, y_train, n_cross_val, n_iter,
+                                                     n_estimators_max)
+                new_best_model.fit(current_x_train, y_train)
+
+                new_accuracy = accuracy_score(y_test, new_best_model.predict(current_x_test))
+
+                if new_accuracy > best_accuracy:
+                    best_model = new_best_model
+                    best_accuracy = new_accuracy
+                    best_features = list(current_x_train.columns)
+
+                evolution_accuracy.append(new_accuracy)
+                removed_features.append(removed_feature)
+            else:
+                break
         else:
             break
-    
+
     time_end = time.time()
-    logging.info(f"Execution time {time_end - time_start} seconds : ")
-    logging.info(f"Best hyperparameters : {best_model.get_params()} ")
-    
-    plt.plot(range(1, len(evolution_accuracy) + 1), evolution_accuracy, marker='o')
-    for i, txt in enumerate(removed_features):
-        plt.annotate(txt, (i + 1, evolution_accuracy[i]), textcoords="offset points", xytext=(0, 10), ha='center', rotation=45, color='red')
-    plt.xlabel('Number of variables withdrawn')
-    plt.ylabel('Accuracy')
-    plt.show()
-    
+    logging.info("Execution time: %s seconds", time_end - time_start)
+    logging.info("Best hyperparameters: %s", best_model.get_params())
+
+    if plot_graph:
+        plt.plot(range(1, len(evolution_accuracy) + 1), evolution_accuracy, marker='o')
+        for i, txt in enumerate(removed_features):
+            plt.annotate(txt, (i + 1, evolution_accuracy[i]), textcoords="offset points", xytext=(0, 10),
+                         ha='center', rotation=45, color='red')
+        plt.xlabel('Number of variables withdrawn')
+        plt.ylabel('Accuracy')
+        plt.show()
+
     return best_model, best_features
-
-def avg_error(num_iterations, test_size_):
-    """Function to visualize the average error of a Random Forest model based on training data, across a specified number of iterations. 
-
-    Args:
-    num_iterations : Number of iterations.
-    test_size_ : Test set proportion.
-
-    Returns:
-        plot_avg_errors : matplotlib.figure.Figure : The Matplotlib Figure object containing the error evolution curve.
-    """
-
-    # List to store errors
-    error_scores = []
-
-    # Number of occurrences per label in each sample (train and test) to balance the labels
-    occurrences_per_label = min(np.bincount(y))
-
-    # Loop over iterations
-    for i in tqdm(range(num_iterations)):
-        # Shuffle data to create a new train and test set in each iteration
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=test_size_, random_state=i, stratify=y)
-
-        # Balance the number of occurrences per label in the train sets only (X_train and y_train)
-        X_train_balanced, _, y_train_balanced, _ = train_test_split(
-            X_train, y_train, train_size=occurrences_per_label * len(np.unique(y_train)), random_state=i, stratify=y_train)
-
-        # Train the model on the new train set
-        model = RandomForestClassifier(n_estimators=N, max_depth=M)
-        model.fit(X_train_balanced, y_train_balanced)
-
-        # Predictions on the new test set
-        y_pred = model.predict(X_test)
-
-        # Calculate the error and add it to the error_scores list
-        error = 1 - accuracy_score(y_test, y_pred)
-        error_scores.append(error * 100)
-
-    # Calculate the average error over iterations
-    average_error = np.mean(error_scores)
-
-    # Plot the error evolution curve
-    plot_avg_errors = plt.figure()
-    plt.plot(range(1, num_iterations + 1), error_scores, marker='o')
-    plt.axhline(y=average_error, color='r', linestyle='--', label=f'Average: {average_error:.4f}')
-    plt.xlabel('Iterations')
-    plt.ylabel('Error')
-    plt.show()
-
-    return plot_avg_errors
